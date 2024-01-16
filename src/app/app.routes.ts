cfd643c13c99ba4ad5fec5396917113600cf41b1
Jorane Membrez <jorane.membrez@outlook.com>
2024-01-16 18:49:43 +0100

    remove duplicate route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,51 @@
 import { Routes } from '@angular/router';
 import { PreloadAllModules, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { TabsPage } from './tabs/tabs.page';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
-  {
-    path: 'layout',
-    loadComponent: () =>
-      import('./layout/layout.page').then((m) => m.LayoutPage),
-  },
-  {
-    path: 'map',
-    loadComponent: () => import('./map/map.page').then((m) => m.MapPage),
-  },
-  {
-    path: 'meeting',
-    loadComponent: () =>
-      import('./meeting/meeting.page').then((m) => m.MeetingPage),
-  },
-  {
-    path: 'profil',
-    loadComponent: () =>
-      import('./profil/profil.page').then((m) => m.ProfilPage),
-  },
+    component: TabsPage,
+    children: [
+      {
+        path: 'layout',
+        loadComponent: () =>
+          import('./layout/layout.page').then((m) => m.LayoutPage),
+      },
+      {
+        path: 'map',
+        loadComponent: () => import('./map/map.page').then((m) => m.MapPage),
+      },
+      {
+        path: 'meeting',
+        loadComponent: () =>
+          import('./meeting/meeting.page').then((m) => m.MeetingPage),
+      },
+      {
+        path: 'profil',
+        loadComponent: () =>
+          import('./profil/profil.page').then((m) => m.ProfilPage),
+      },
 
-  {
-    path: 'login',
-    loadChildren: () =>
-      import('./auth/login/login.page').then((m) => m.LoginPage),
-  },
-  {
-    path: 'sigup',
-    loadComponent: () =>
-      import('./auth/sigup/sigup.page').then((m) => m.SignupPage),
-  },
-  {
-    path: 'edit-profile',
-    loadComponent: () =>
-      import('./edit-profile/edit-profile.page').then((m) => m.EditProfilePage),
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./auth/login/login.page').then((m) => m.LoginPage),
+      },
+      {
+        path: 'sigup',
+        loadComponent: () =>
+          import('./auth/sigup/sigup.page').then((m) => m.SignupPage),
+      },
+      {
+        path: 'edit-profile',
+        loadComponent: () =>
+          import('./edit-profile/edit-profile.page').then(
+            (m) => m.EditProfilePage
+          ),
+      },
+    ],
   },
 ];
 
